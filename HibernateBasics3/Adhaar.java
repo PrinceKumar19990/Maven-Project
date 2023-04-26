e45b423838f96765657276e26884983e36e66093
PrinceKumar19990 <122788054+PrinceKumar19990@users.noreply.github.com>
2023-04-27 00:10:12 +0530

    Add files via upload

---
+++ HibernateBasics3/Adhaar.java
@@ -0,0 +1,31 @@
+package Hibernate.cfg.xml2;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="adharr_infor")
+public class Adhaar {
+@Id
+@Column(name="adhaar_id")
+private int id;
+private int adhaarNum;
+public int getAdhaarNum() {
+return adhaarNum;
+}
+public void setAdhaarNum(int adhaarNum) {
+this.adhaarNum = adhaarNum;
+}
+public int getId() {
+return id;
+}
+public void setId(int id) {
+this.id = id;
+}
+public Adhaar(int adhaarNum) {
+super();
+this.adhaarNum = adhaarNum;
+}
+}
---
+++ HibernateBasics3/MainApp.java
@@ -0,0 +1,20 @@
+package Hibernate.cfg.xml2;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+public class MainApp {
+public static void main(String[] args) {
+Person deepak = new Person("deepak kumar","Delhi");
+Adhaar ada = new Adhaar(71122345);
+deepak.setAdhar(ada);
+Transaction txn;
+try(Session session=HibernateUtil.getSessionFactory().openSession()){
+txn = session.beginTransaction();
+session.save(deepak);
+txn.commit();
+txn=session.beginTransaction();
+Adhaar Prince= session.get(Adhaar.class, 0);
+Prince.setAdhaarNum(0);
+}
+}
+}
