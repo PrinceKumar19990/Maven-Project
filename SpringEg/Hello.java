20bf9ebf9960dba0682114b7fe023aedea7b59b2
PrinceKumar19990 <122788054+PrinceKumar19990@users.noreply.github.com>
2023-04-28 23:48:48 +0530

    Add files via upload

---
+++ SpringEg/Hello.java
@@ -0,0 +1,11 @@
+package com.spring.ex1;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Hello {
+public static void main(String[] args) {
+	ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("application.Context.xml");
+    HelloService obj=(HelloService)context.getBean("HelloService");
+    obj.hello();
+    context.close();
+   }
+ }
---
+++ SpringEg/HelloService.java
@@ -0,0 +1,7 @@
+package com.spring.ex1;
+
+public class HelloService {
+public void hello() {
+	System.out.println("Welcome,Rahul_Kumar.....");
+}
+}
