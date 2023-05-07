707f2ee346441d27d5408c43d9863b5e77c66f11
PrinceKumar19990 <122788054+PrinceKumar19990@users.noreply.github.com>
2023-05-07 23:38:21 +0530

    Add files via upload

---
+++ Annoation/src/main/java/com/LifeCycle/AppConfig.java
@@ -0,0 +1,10 @@
+package com.LifeCycle;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.LifeCycle")
+public class AppConfig {
+}
+
